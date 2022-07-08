791cc7365f33913c3e0eef5852d6d2bd51a3c9f8
Alexxxsandoor <v.verstukov14@gmail.com>
2022-07-08 20:29:37 +0300

    end of test task

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export function Header() {
+	return (
+		<div className="header">
+			<h1>Exchange rates</h1>
+			<a className="Repo" href="https://github.com/Alexxxsandoor/Exchange-rates">Repo</a>
+		</div>
+	)
+}
